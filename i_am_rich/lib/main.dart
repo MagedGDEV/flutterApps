2fbe5f5a77e6de84918a1662e1455126f48c937a
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-07-12 18:21:18 +0300

    Use scaffold to create app bar and insert an image

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -3,8 +3,16 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World!"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Image(
+          image: NetworkImage(
+              'https://www.seiu1000.org/sites/main/files/main-images/camera_lense_0.jpeg'),
+        ),
+        backgroundColor: Colors.blueGrey,
       ),
     ),
   );
