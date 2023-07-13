778764c8a3a1bd0a8814c5dfa00cd37cf93e8778
MagedGDEV <98350741+MagedGDEV@users.noreply.github.com>
2023-07-13 05:48:21 +0300

    remove the project

--- i_am_poor/android/app/src/main/kotlin/com/example/i_am_poor/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.i_am_poor
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- i_am_rich/android/app/src/main/kotlin/com/example/i_am_rich/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.i_am_rich
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
